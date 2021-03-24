be4f4e9b7450dc33428f50432281acf9dbdc3619
DropBear25 <michelle25@live.co.uk>
2021-03-24 14:57:23 +0000

    animation aim

---
+++ Survival/Assets/PlayerWeapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+
+    private Animator Anim;
+
+    public bool Gun;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Gun == true)
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                Anim.SetBool("Aim", true);
+                
+                 
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                Anim.SetBool("Aim", false);
+
+            }
+        }
+
+            
+
+       
+
+
+
+    }
+}
