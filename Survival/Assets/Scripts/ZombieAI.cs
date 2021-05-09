5f82dbfc8525ea98bdf0afb22dbbabc5e88b8846
DropBear25 <michelle25@live.co.uk>
2021-05-09 21:08:20 +0100

    Zombie Script

---
+++ Survival/Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAI : MonoBehaviour
+{
+
+    public GameObject target;
+    Animator anim;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
+
+        anim.SetBool("isWalking", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isAttacking", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", true);
+        }
+         
+    }
+}
