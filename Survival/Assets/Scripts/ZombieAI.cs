893b84fdd5edfd22aa23b129cea5c4fd6f14a66a
DropBear25 <michelle25@live.co.uk>
2021-05-11 19:19:42 +0100

    Zombie Animation

--- Survival/Assets/Scripts/ZombieAI.cs
+++ Survival/Assets/Scripts/ZombieAI.cs
@@ -7,11 +7,13 @@ public class ZombieAI : MonoBehaviour
 {
 
     public GameObject target;
+    public float walkingSpeed;
+    public float runningSpeed;
     Animator anim;
     NavMeshAgent agent;
 
 
-    enum STATE { IDLE, WANDER, ATTACK, CHASE, DEAD};
+    enum STATE { IDLE, WANDER, ATTACK, CHASE, DEAD };
     STATE state = STATE.IDLE; 
 
 
@@ -19,26 +21,102 @@ public class ZombieAI : MonoBehaviour
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
-        anim = this.GetComponent<Animator>();
+        anim = this.GetComponent<Animator>();  
+    }
 
-        anim.SetBool("isWalking", true);
-      
+    void TurnOffTriggers()
+    {
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isAttacking", false);
+        anim.SetBool("isRunning", false);
+        anim.SetBool("isDead", false);
+        
+    }
+
+    float DistanceToPlayer()
+    {
+        return Vector3.Distance(target.transform.position, this.transform.position);
+    }
+
+    bool CanSeePlayer()
+    {
+        if (DistanceToPlayer() < 10)
+            return true;
+        return false;
+       
+    }
+
+    bool ForgetPlayer()
+    {
+        if (DistanceToPlayer() > 10)
+            return true;
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);
-        if(agent.remainingDistance > agent.stoppingDistance)
+      if(target == null)
         {
-            anim.SetBool("isWalking", true);
-            anim.SetBool("isAttacking", false);
+           target = GameObject.FindWithTag("Player");
+            return;
         }
-        else
+      switch (state)
         {
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isAttacking", true);
+            case STATE.IDLE:
+                if (CanSeePlayer()) state = STATE.CHASE;
+                else if(Random.Range(0, 6000) < 5)
+                state = STATE.WANDER;
+                break;
+            case STATE.WANDER:
+                if (!agent.hasPath)
+                {
+                    float newX = this.transform.position.x + Random.Range(-5, 5);
+                    float newZ = this.transform.position.z + Random.Range(-5, 5);
+                    float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX, 0, newZ));
+                    Vector3 dest = new Vector3(newX, newY, newZ);
+                    agent.SetDestination(dest);
+                    agent.stoppingDistance = 0;
+                    TurnOffTriggers();
+                    agent.speed = walkingSpeed;
+                    anim.SetBool("isWalking", true);
+                }
+                if (CanSeePlayer()) state = STATE.CHASE;
+                else if (Random.Range(0, 6000) < 5)
+                {
+                    state = STATE.IDLE;
+                    TurnOffTriggers();
+                    agent.ResetPath();
+                   
+                }
+                break;
+            case STATE.CHASE:
+                agent.SetDestination(target.transform.position);
+                agent.stoppingDistance = 5;
+                TurnOffTriggers();
+                agent.speed = runningSpeed;
+                anim.SetBool("isRunning", true);
+
+                if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+                {
+                    state = STATE.ATTACK;   
+                }
+                if (ForgetPlayer())
+                {
+                    state = STATE.WANDER;
+                    agent.ResetPath();
+                }
+
+                break;
+            case STATE.ATTACK:
+                TurnOffTriggers();
+                anim.SetBool("isAttacking", true);
+                this.transform.LookAt(target.transform.position);
+                if (DistanceToPlayer() > agent.stoppingDistance + 2)
+                    state = STATE.CHASE;
+                break;
+            case STATE.DEAD:
+                break;
         }
-         
     }
 }
